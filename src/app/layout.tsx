5e1f6727f6b68dd6d0a2fb3f2e1f5210a6ed647d
Ditya <gdevelop351@gmail.com>
2025-12-29 21:12:00 +0800

    perbaikan seo

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
 import StructuredData from "@/components/StructuredData";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Geist, Geist_Mono, Inter } from "next/font/google";
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
